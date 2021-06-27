e56582968c7ee5d3af79c19d175ae880d70e35af
mhashim6 <m6hashim@gmail.com>
2021-06-27 19:03:38 +0200

    initial

---
+++ dowload.py
@@ -0,0 +1,19 @@
+
+import urllib.request as remote
+import os
+import requests
+
+user_agent = {'User-agent': 'Mozilla/5.0'}
+
+BASE_URL = "https://cdn.islamic.network/quran/audio";
+def download(bitrate, edition, start=1, end=6236):
+    try:
+        os.mkdir(f'{bitrate}')
+        os.mkdir(f'{bitrate}/{edition}')
+    except:
+        pass
+    for ayah in range(start, end):
+        res = requests.get(f'https://cdn.islamic.network/quran/audio/{bitrate}/{edition}/{ayah}.mp3', headers = user_agent)
+        open(f'{bitrate}/{edition}/{ayah}.mp3', 'wb').write(res.content)
+
+download(64, 'ar.husary', 1, 8)
